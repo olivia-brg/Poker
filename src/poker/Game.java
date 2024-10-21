7f5b93e1304bdc4de1fb7b1fe3dd8be682bd1e93
Olivia <olivia.bergaglia@gmail.com>
2024-10-21 18:31:37 +0200

    nothing important

--- src/poker/Game.java
+++ src/poker/Game.java
@@ -17,15 +17,54 @@ public class Game {
         this.communityCards = new ArrayList<>();
     }
 
+    public void playersTurn(Player player, Scanner inputScanner) {
+
+        if (!player.hasFolded()) {
+
+            System.out.println(player.getName() + " you have " + player.getTokens() + "\nDo you want to see your cards ? (Y/N)");
+            String choice = inputScanner.nextLine();
+
+            if (choice.equalsIgnoreCase("Y")) {
+                System.out.println("Your cards are : " + player.getHand() + "\n");
+            } else {
+                System.out.println(player.getName() + " chose not to see their cards." + "\n");
+            }
+
+            System.out.println("What do you want to do ? \n1 : Fold\n2 : Call\n3 : Raise");
+
+            int bet = inputScanner.nextInt();
+            while (bet < 1 || bet > 3) {
+                bet = inputScanner.nextInt();
+                System.out.println("Not a valid choise, please try again");
+            }
+            inputScanner.nextLine();
+            switch (bet) {
+                case 1 -> {
+                    player.setFolded(true);
+                    System.out.println(player.getName() + " fold\n");
+                }
+                case 2 -> {
+                    System.out.println("call\n");
+                }
+                case 3 -> {
+                    System.out.println("raise\n");
+                }
+            }
+        } else {
+            System.out.println(player.getName() + " has folded !");
+        }
+    }
+
     public void drawCommunityCards(int numberOfCards) {
         communityCards.addAll(Deck.Deal(numberOfCards));
     }
 
     public void printCommunityCards() {
-        System.out.println("Community Cards :");
+        System.out.println("\nCommunity Cards :");
         for (Card card : communityCards) {
             System.out.println(card);
         }
+        System.out.println("\n");
     }
 
     public static void main(String[] args) {
@@ -54,48 +93,32 @@ public class Game {
         Game game = new Game(players);
 
         for (Player player : players) {
-            if (player.hasFolded()) {
-
-                System.out.println(player.getName() + " you have " + player.getTokens() + "\nDo you want to see your cards ? (Y/N)");
-                String choice = inputScanner.nextLine();
-
-                if (choice.equalsIgnoreCase("Y")) {
-                    System.out.println("Your cards are : " + player.getHand() + "\n");
-                } else {
-                    System.out.println(player.getName() + " chose not to see their cards." + "\n");
-                }
-
-                System.out.println("What do you want to do ? \n1 : Fold\n2 : Call\n3 : Raise");
-
-                int bet = inputScanner.nextInt();
-                while (bet < 1 || bet > 3) {
-                    bet = inputScanner.nextInt();
-                    System.out.println("Not a valid choise, please try again");
-                }
-                inputScanner.nextLine();
-                switch (bet) {
-                    case 1 -> {
-                        player.setFolded(true);
-                        System.out.println(player.getName() + " has folded\n");
-                    }
-                    case 2 -> {
-                        System.out.println("call\n");
-                    }
-                    case 3 -> {
-                        System.out.println("raise\n");
-                    }
-                }
-            }
-
+            game.playersTurn(player, inputScanner);
         }
-        System.out.println(game.players);
-        game.drawCommunityCards(3);
 
-        game.drawCommunityCards(1);
+        game.drawCommunityCards(3);
+        game.printCommunityCards();
+
+        for (Player player : players) {
+            game.playersTurn(player, inputScanner);
+        }
 
         game.drawCommunityCards(1);
         game.printCommunityCards();
 
-    }
+        for (Player player : players) {
+            game.playersTurn(player, inputScanner);
+        }
 
+        game.drawCommunityCards(1);
+        game.printCommunityCards();
+
+        for (Player player : players) {
+            game.playersTurn(player, inputScanner);
+        }
+        
+        for (Player player : players) {
+            player.getHand();
+        }
+    }
 }
