c0114391f5b14bc7945a43024cc70904962d01b1
Olivia <olivia.bergaglia@gmail.com>
2024-10-25 03:26:38 +0200

    refacto

--- src/poker/Game.java
+++ src/poker/Game.java
@@ -4,20 +4,23 @@ import java.util.*;
 
 public class Game {
 
-    private final List<Player> players;
+    private LinkedList<Player> AllPlayers;
+    private LinkedList<Player> playersInGame;
     private final List<Card> communityCards;
     private int smallBlind;
     private int currentMinBet;
     private int dealerIndex;
     private int totalBetTokens;
-    private int playersInGame;
+    // private int playersInGame;
 
-    public Game(List<Player> players, int numberOfPlayers) {
-        this.players = players;
+    public Game(LinkedList<Player> players, int numberOfPlayers) {
+
+        this.AllPlayers = new LinkedList<>(players);
+        this.playersInGame = AllPlayers;
         this.communityCards = new ArrayList<>();
         this.smallBlind = 2;
         this.currentMinBet = this.smallBlind * 2;
-        this.playersInGame = numberOfPlayers;
+        // this.playersInGame = numberOfPlayers;
     }
 
     public void playersTurn(Player player, Scanner inputScanner) {
@@ -51,7 +54,7 @@ public class Game {
 
                 case 1 -> {
                     player.setFolded(true);
-                    playersInGame--;
+                    playersInGame.remove(player);
                     System.out.println("\n" + player.getName() + " fold\n\n┌------------------------------------┐");
                 }
                 case 2 -> {
@@ -81,8 +84,10 @@ public class Game {
 
                     player.betTokens(bet);
                     totalBetTokens += bet;
-                    if (currentMinBet < player.getActualBet()) currentMinBet = player.getActualBet();
-                    
+                    if (currentMinBet < player.getActualBet()) {
+                        currentMinBet = player.getActualBet();
+                    }
+
                     System.out.println("\n You raise by " + addToBet + " tokens\n\n┌------------------------------------┐\n|        " + player.getName() + " is at " + player.getActualBet() + " tokens bet");
                 }
             }
@@ -105,32 +110,37 @@ public class Game {
 
     public Player getDealer(Integer... nth) {
         Integer n = nth.length > 0 ? nth[0] : 0;
-        return players.get((dealerIndex + n )% players.size());
+        return playersInGame.get((dealerIndex + n) % playersInGame.size());
     }
 
     public void playRound(Scanner inputScanner) {
-        int currentPlayerIndex = (dealerIndex + 3) % players.size();
+        LinkedList<Player> playersInTurn = new LinkedList<>(playersInGame);
+        int currentPlayerIndex = (dealerIndex + 3) % playersInTurn.size();
 
-        for (int i = 0; i < players.size(); i++) {
-            if (playersInGame < 2) break;
-            Player currentPlayer = players.get(currentPlayerIndex);
+        for (int i = 1; i < playersInTurn.size(); i++) {
+            if (playersInGame.size() < 2) {
+                break;
+            }
+            Player currentPlayer = playersInTurn.get(currentPlayerIndex);
             playersTurn(currentPlayer, inputScanner);
 
-            currentPlayerIndex = (currentPlayerIndex + 1) % players.size();
+            currentPlayerIndex = (currentPlayerIndex + 1) % playersInTurn.size();
         }
     }
 
     public static void main(String[] args) {
         Scanner inputScanner = new Scanner(System.in);
-        List<Player> players = new ArrayList<>();
+        LinkedList<Player> playersEntry = new LinkedList<>();
         int numberOfPlayers;
 
-        System.out.println("\u001B[30m" + "\u001B[47m" + "\nHow many player there is ?");
+        System.out.println("\u001B[30m\u001B[47m\nHow many player there is ?");
         do {
             try {
                 String s = inputScanner.nextLine();
                 numberOfPlayers = Integer.parseInt(s);
-                if (numberOfPlayers >= 3) break;
+                if (numberOfPlayers >= 3) {
+                    break;
+                }
                 System.out.println("\nYou need to be 3 or more players, please try again");
             } catch (NumberFormatException e) {
                 System.out.println("\nCouldn't parse input, please try again");
@@ -144,24 +154,24 @@ public class Game {
             String userName = inputScanner.nextLine();
 
             Player player = new Player(userName);
-            players.add(player);
+            playersEntry.add(player);
         }
 
         String playAgain;
         int gameNumber = 0;
-        
+
         do {
-            Game game = new Game(players, numberOfPlayers);
-            
+            Game game = new Game(playersEntry, numberOfPlayers);
+
             game.dealerIndex += gameNumber;
             gameNumber++;
 
-            System.out.println("\n------------------------------------\n------------- Game n°" + gameNumber + " -------------\n------------------------------------\n\n" +
-                                "Dealer : " + game.getDealer().getName());
+            System.out.println("\u001B[30m\u001B[47m\n------------------------------------\n------------- Game n°" + gameNumber + " -------------\n------------------------------------\n\n"
+                    + "Dealer : " + game.getDealer().getName());
 
             Deck.CreateDeck();
 
-            for (Player player : players) {
+            for (Player player : playersEntry) {
                 player.setFolded(false);
                 player.setHand(Deck.Deal(2));
                 player.resetActualBet();
@@ -184,15 +194,16 @@ public class Game {
                 boolean newTurn;
                 do {
                     game.playRound(inputScanner);
-                    
+
                     LinkedList<Integer> playersActualBet = new LinkedList<>();
-                    for (Player player : players) {
-                        if (!player.hasFolded()) playersActualBet.push(player.getActualBet());
+                    for (Player player : game.playersInGame) {
+                        playersActualBet.add(player.getActualBet());
+
                     }
 
                     newTurn = false;
-                    for (int i = 1; i < game.playersInGame; i++) {
-                        if (!Objects.equals(playersActualBet.get(0), playersActualBet.get(i)) && game.playersInGame >= 2) {
+                    for (int i = 0; i < game.playersInGame.size(); i++) {
+                        if (!Objects.equals(playersActualBet.get(0), playersActualBet.get(i)) && game.playersInGame.size() >= 2) {
                             newTurn = true;
                             break;
                         }
@@ -200,7 +211,7 @@ public class Game {
 
                 } while (newTurn);
 
-                if (game.playersInGame >= 2) {
+                if (game.playersInGame.size() >= 2) {
                     if (round == 0) {
                         game.setCommunityCards(3); // Flop
                     } else {
@@ -208,8 +219,8 @@ public class Game {
                     }
                     game.getCommunityCards();
                 } else {
-                    for (Player player : players) {
-                        if (!player.hasFolded()) winner = player;
+                    for (Player player : game.playersInGame) {
+                        winner = player;
                     }
                 }
 
@@ -217,26 +228,28 @@ public class Game {
 
             game.playRound(inputScanner);
 
-            players.toString();
-            
+            if (game.playersInGame.size() > 1) {
+                for (int i = 0; i < game.playersInGame.size(); i++) {
+                    playersEntry.get(i).getHand();
+                    System.out.println("Player " + (i + 1) + " - " + game.playersInGame.get(i).getName() + " : " + game.playersInGame.get(i).getHand());
+                }
+            }
 
             winner.earnedTokens(game.totalBetTokens);
             System.out.println("\n-_-_-_-_-_- Well played " + winner.getName() + ", you won " + (game.totalBetTokens - winner.getActualBet()) + " tokens ! -_-_-_-_-_-");
 
-            LinkedList<Player> playersRanking = new LinkedList<>();
-            for (Player player : players) {
-                playersRanking.push(player);
-            }
-
-            playersRanking.sort(Comparator.comparingInt(Player::getTokens).reversed());
             System.out.println("\nPlayers ranking :\n");
 
-            for (int i = 0; i < playersRanking.size(); i++) {
-                System.out.println((i+1) + " : " + playersRanking.get(i).toString());
-            }            
+            LinkedList<Player> playersRanking = new LinkedList<>(playersEntry);
+            playersRanking.sort(Comparator.comparingInt(Player::getTokens).reversed());
 
-            System.out.println("\n\nPlay again ? (Y for yes)");
+            for (int i = 0; i < playersRanking.size(); i++) {
+                System.out.println((i + 1) + " : " + playersRanking.get(i).toString());
+            }
+
+            System.out.println("\n\nPlay again ? (Y for yes / Enter for no)");
             playAgain = inputScanner.nextLine();
+            System.out.println("\u001B[0m");
 
         } while (playAgain.equalsIgnoreCase("Y"));
     }
